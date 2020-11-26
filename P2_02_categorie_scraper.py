587d7e652e16808479d26f26d26276df7232c839
harrisafoster <harrisafoster@gmail.com>
2020-11-26 11:19:32 +0100

    définir une fonction pour récupérer et parser les données d'un URL pour l'utiliser plus tard.

--- P2_02_categorie_scraper.py
+++ P2_02_categorie_scraper.py
@@ -6,3 +6,8 @@ from word2number import w2n
 
 main_url = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
 
+def getAndParseURL(url):
+	sauce = requests.get(url)
+	soup = BeautifulSoup(sauce.text, features='html.parser')
+	return(soup)
+

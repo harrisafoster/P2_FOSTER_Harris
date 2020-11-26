81b1f4aab3d4e190bf56c16289218c97c3134b80
harrisafoster <harrisafoster@gmail.com>
2020-11-26 10:50:04 +0100

    Etablissement du variable img_url qui cherchera la réference SRC de l'image et remplacera "index.html" de l'URL principal avec ladite SRC.

--- P2_01_page_scraper.py
+++ P2_01_page_scraper.py
@@ -22,3 +22,6 @@ price_without_tax = tds[2].text
 price_with_tax = tds[3].text
 
 nb_in_stock = re.sub("[^0-9]", "", soup.find("p", class_ = "instock availability").text)
+
+img_url = main_url.replace("index.html", "") + soup.find("img").get("src")
+

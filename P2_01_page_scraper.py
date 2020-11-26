a0a62489519d8582932784d9f0ec586b2be6ec56
harrisafoster <harrisafoster@gmail.com>
2020-11-26 10:47:56 +0100

    Etablissement du variable nb_in_stock qui cherchera les données par classe 'instock availability' et capturera uniquement le nombre sans le texte.

--- P2_01_page_scraper.py
+++ P2_01_page_scraper.py
@@ -20,3 +20,5 @@ else:
 tds = soup.find_all('td')
 price_without_tax = tds[2].text
 price_with_tax = tds[3].text
+
+nb_in_stock = re.sub("[^0-9]", "", soup.find("p", class_ = "instock availability").text)

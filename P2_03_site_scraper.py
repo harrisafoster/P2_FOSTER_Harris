9f90d675c69718cc40bc61d1f3a519f47f1c5735
harrisafoster <harrisafoster@gmail.com>
2020-11-26 11:51:00 +0100

    Etablissement du variable main_url contenant l'URL principal du site.
    Défintion des deux fonctions utilisées précédemment pour récupérer, concaténer, et parser les URLs nécéssaires.

--- P2_03_site_scraper.py
+++ P2_03_site_scraper.py
@@ -7,3 +7,14 @@ import shutil
 import os
 import time
 
+main_url = 'http://books.toscrape.com/index.html'
+
+def getAndParseURL(url):
+	sauce = requests.get(url)
+	soup = BeautifulSoup(sauce.text, features='html.parser')
+	return(soup)
+
+def getBooksURLs(url):
+	soup = getAndParseURL(url)
+	return(["/".join(url.split("/")[:-1]) + "/" + x.div.a.get('href') for x in soup.find_all("article", class_="product_pod")])
+

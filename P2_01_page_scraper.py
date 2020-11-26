ee5468dc94098bef015c5f6e439026c93db286ba
harrisafoster <harrisafoster@gmail.com>
2020-11-26 10:28:01 +0100

    Etablissement de l'URL de la page, définition de la source HTML avec requests, défintion du variable "soup" avec BeautifulSoup pour récupérer et parser les données.

--- P2_01_page_scraper.py
+++ P2_01_page_scraper.py
@@ -3,3 +3,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 from word2number import w2n
+
+main_url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+
+sauce = requests.get(main_url)
+soup = BeautifulSoup(sauce.text, features='html.parser')

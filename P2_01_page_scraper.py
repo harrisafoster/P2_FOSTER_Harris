afb8bada595fd914c4fad0f1a8dd73ab250f5041
harrisafoster <harrisafoster@gmail.com>
2020-11-26 10:31:45 +0100

    Définition du variable 'name' qui servira à trouver le titre du livre
    Définition du variable 'description' qui cherchera un tag 'p' d'une longeur suffisante pour être la déscription et ensuite la capturer. Sinon, il capturera "No description available".

--- P2_01_page_scraper.py
+++ P2_01_page_scraper.py
@@ -8,3 +8,11 @@ main_url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.
 
 sauce = requests.get(main_url)
 soup = BeautifulSoup(sauce.text, features='html.parser')
+
+name = soup.find("div", class_ = re.compile("product_main")).h1.text
+
+paragraphs = soup.find_all('p')
+if len(paragraphs[3].text) > 10:
+	description = paragraphs[3].text
+else: 
+	description = "No description available."
